3db13ef6cf26657c28b81c5221e7c0ef8f412341
Qunnderrie Snelling <qunnderrie@gmail.com>
2022-08-24 18:47:10 -0400

    First react commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello I'm Qunnderrie Snelling</h1>
+        <p>Coding is my passion</p>
+        <ul>
+          <li>I love to go the parks</li>
+          <li>Playing basketball</li>
+          <li>Play video games</li>
+        </ul>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
